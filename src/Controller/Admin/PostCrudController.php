1d5cdd95dcdfbce54571326a03cbcdec9f10d117
mbouridev <mbouri.dev@gmail.com>
2022-04-05 14:58:48 +0200

    stash

---
+++ src/Controller/Admin/PostCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class PostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Post::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->disable(Action::NEW);
+        
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
